02218bc12d0e1e8c83dedc3250c2b188ed8f84b1
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-26 00:05:16 +0300

    added Int and Bool Generators in plugin

---
+++ DTOGenerator/PluginGenerators/BoolGenerator.cs
@@ -0,0 +1,29 @@
+﻿using DTOGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginGenerators
+{
+    public class BoolGenerator : IGenerator
+    {
+        Random random;
+
+        public BoolGenerator()
+        {
+            random = new Random();
+        }
+
+        public object Generate()
+        {
+            return random.Next(1) == 1 ? true : false;
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(bool);
+        }
+    }
+}
---
+++ DTOGenerator/PluginGenerators/IntGenerator.cs
@@ -0,0 +1,29 @@
+﻿using DTOGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginGenerators
+{
+    public class IntGenerator : IGenerator
+    {
+        Random random;
+
+        public IntGenerator()
+        {
+            random = new Random();
+        }
+
+        public object Generate()
+        {
+            return random.Next(int.MinValue, int.MaxValue);
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(int);
+        }
+    }
+}
