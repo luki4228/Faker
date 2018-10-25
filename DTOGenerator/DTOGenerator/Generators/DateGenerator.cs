de1414260fc6c70ff03d488ef585656663a8d438
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-25 22:35:40 +0300

    added List and Date Generators

---
+++ DTOGenerator/DTOGenerator/Generators/DateGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO.Generators
+{
+    public class DateGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ DTOGenerator/DTOGenerator/Generators/ListGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorDTO.Generators
+{
+    public class ListGenerator : ICollectionGenerator
+    {
+        public object Generate(Type t)
+        {
+            object res = Activator.CreateInstance(typeof(List<>).MakeGenericType(t));
+
+            ((IList)res).Add(ParameterGenerator.Generate(t));
+
+            return res;
+        }
+    }
+}
