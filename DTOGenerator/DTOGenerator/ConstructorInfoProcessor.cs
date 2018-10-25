94fcd05c30cd51cf3e4c08597186850dba2f4a26
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-25 21:01:20 +0300

    implemented class ConstructorInfoProcessor

---
+++ DTOGenerator/DTOGenerator/ConstructorInfoProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOGenerator
+{
+    internal static class ConstructorInfoProcessor
+    {
+        internal static ConstructorInfo GetMaxParameterizedConstructor(List<ConstructorInfo> allConstructors)
+        {
+            if (allConstructors.Count == 0)
+            {
+                throw new InvalidOperationException("Empty list");
+            }
+
+            allConstructors = allConstructors.OrderBy(item => item.GetParameters().Length).ToList();
+            return allConstructors[0];
+        }
+    }
+}
