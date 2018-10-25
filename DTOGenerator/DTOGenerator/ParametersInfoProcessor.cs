4089e99ba72ebc9b12185e5ccf5cf2a1655e2941
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-25 21:07:00 +0300

    implemented methods that check parameters

---
+++ DTOGenerator/DTOGenerator/ParametersInfoProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOGenerator
+{
+    internal static class ParametersInfoProcessor
+    {
+        internal static List<ParameterInfo> GetParametersInfo(ConstructorInfo constructor)
+        {
+            return constructor.GetParameters().ToList();
+        }
+
+        internal static bool IsParameterSimple(ParameterInfo parameter)
+        {
+            Type type = parameter.GetType();
+            return type.IsPrimitive || type.Equals(typeof(string));
+        }
+    }
+}
