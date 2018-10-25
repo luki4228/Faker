11e1ae99b20cddce53c0fed3d47e8af301f65b11
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-25 22:25:14 +0300

    implemented Faker Class and added Parameter Generater

--- DTOGenerator/DTOGenerator/Faker.cs
+++ DTOGenerator/DTOGenerator/Faker.cs
@@ -10,9 +10,56 @@ namespace DTOGenerator
     public class Faker
     {
         public Faker() { }
+
+        public object CreateByConstructor(ConstructorInfo constructor)
+        {
+            List<ParameterInfo> parameters = ParametersInfoProcessor.GetParametersInfo(constructor);
+            object[] tmpParams = new object[parameters.Count];
+            int i = 0;
+            foreach (ParameterInfo parameterInfo in parameters)
+            {
+                tmpParams[i] = ParameterGenerator.Generate(parameterInfo.GetType(), this);
+            }
+            return constructor.Invoke(tmpParams);
+        }
+
+        public object CreateByFieldsAndProperties(Type t)
+        {
+            object res = Activator.CreateInstance(t);
+            List<FieldInfo> fields = ClassInfo.GetClassFieldsInfo(t);
+            List<PropertyInfo> settableProperties = PropertiesInfoProcessor.GetSettableProperties(ClassInfo.GetClassPropertiesInfo(t));
+            foreach (FieldInfo field in fields)
+            {
+                field.SetValue(res, ParameterGenerator.Generate(field.FieldType, this));
+            }
+            foreach (PropertyInfo property in settableProperties)
+            {
+                property.SetValue(res, ParameterGenerator.Generate(property.PropertyType, this));
+            }
+            return res;
+        }
+
+        public object Create(Type t)
+        {
+            ParameterGenerator.AddToRecursionControlList(t);
+            Object result;
+            List<ConstructorInfo> constructors = ClassInfo.GetClassConstructorsInfo(t);
+            if (constructors.Count > 1)
+            {
+                ConstructorInfo bestConstructor = ConstructorInfoProcessor.GetMaxParameterizedConstructor(constructors);
+                result = CreateByConstructor(bestConstructor);
+            }
+            else
+            {
+                result = CreateByFieldsAndProperties(t);
+            }
+            return result;
+        }
+
         public T Create<T>()
         {
-            throw new NotImplementedException();
+            ParameterGenerator.ClearRecursionControlList();
+            return (T)Create(typeof(T));
         }
     }
 }
---
+++ DTOGenerator/DTOGenerator/ParameterGenerator.cs
@@ -0,0 +1,60 @@
+﻿using DTOGenerator.Generators;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOGenerator
+{
+    internal static class ParameterGenerator
+    {
+        private static Dictionary<Type, IGenerator> generatorMethods;
+        private static List<Type> recursionControl;
+        static ParameterGenerator()
+        {
+            generatorMethods = new Dictionary<Type, IGenerator>();
+            generatorMethods.Add(typeof(double), new DoubleGenerator());
+            generatorMethods.Add(typeof(uint), new UIntGenerator());
+            generatorMethods.Add(typeof(float), new FloatGenerator());
+            generatorMethods.Add(typeof(char), new CharGenerator());
+            generatorMethods.Add(typeof(string), new StringGenerator());
+            generatorMethods.Add(typeof(long), new LongGenerator());
+            recursionControl = new List<Type>();
+        }
+        public static void AddToRecursionControlList(Type t)
+        {
+            recursionControl.Add(t);
+        }
+        public static void ClearRecursionControlList()
+        {
+            recursionControl.Clear();
+        }
+        internal static object Generate(Type parameterType, Faker faker)
+        {
+            if (generatorMethods.ContainsKey(parameterType))
+            {
+                return generatorMethods[parameterType].Generate();
+            }
+            else
+            {
+                if (recursionControl.Contains(parameterType))
+                {
+                    throw new InvalidOperationException("Cyclic objects! First DTO contains second DTO as field, second DTO contains first as field.");
+                }
+                else
+                {
+                    if (DTOChecker.IsDto(parameterType))
+                    {
+                        return faker.Create(parameterType);
+                    }
+                    else
+                    {
+                        return parameterType.IsValueType ? Activator.CreateInstance(parameterType) : null;
+                    }
+                }
+            }
+        }
+    }
+}
