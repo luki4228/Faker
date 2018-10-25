14bf242dec6c0d36a41909fbde5213a8715dcf69
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-25 21:04:40 +0300

    implemented method that check property

---
+++ DTOGenerator/DTOGenerator/PropertiesInfoProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOGenerator
+{
+    internal static class PropertiesInfoProcessor
+    {
+        internal static List<PropertyInfo> GetSettableProperties(List<PropertyInfo> allProperties)
+        {
+            return new List<PropertyInfo>(allProperties.Where(item => !item.GetSetMethod().IsPrivate));
+        }
+    }
+}
