5f12f027049651b083f07a4783ed407a80b3b5b1
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-25 20:56:46 +0300

    implemented ClassInfo

---
+++ DTOGenerator/DTOGenerator/ClassInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOGenerator
+{
+    public static class ClassInfo
+    {
+        public static List<ConstructorInfo> GetClassConstructorsInfo(Type t)
+        {
+            return new List<ConstructorInfo>(t.GetConstructors());
+        }
+
+        public static List<PropertyInfo> GetClassPropertiesInfo(Type t)
+        {
+            return new List<PropertyInfo>(t.GetProperties());
+        }
+
+        public static List<FieldInfo> GetClassFieldsInfo(Type t)
+        {
+            return new List<FieldInfo>(t.GetFields());
+        }
+    }
+}
